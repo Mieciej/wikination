b7f331599c1b6205e38d1829b4b9ad86706d932f
Maciej Janicki <maciej.ja@proton.me>
2024-12-21 22:36:32 +0100

    Update: construct term idf-tf table using sqlite.

--- tf-idf.cpp
+++ tf-idf.cpp
@@ -1,68 +1,85 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <sqlite3.h>
+#include <cstdlib>
 #include <unordered_map>
-#include <map>
-#include <filesystem>
-#include <cassert>
+#include <vector>
 #include <cmath>
 
+sqlite3* db;
+void check_resut(int rc) {
+  if (rc != SQLITE_OK) {
+    std::cerr << "Failed to execute query: " << sqlite3_errmsg(db) << std::endl;
+    sqlite3_close(db);
+    std::exit(1);
+  }
+}
+
+
 int main(){
-  std::ifstream file("lemma/dict.txt");
-  if(!file.is_open()) {
-    std::cerr << "Failed to open lemma/dict.txt file" << std::endl;
+  int rc = sqlite3_open("bow.db", &db);
+  if (rc) {
+    std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
     return 1;
   }
-  std::string line;
-  std::getline(file, line);
-  int n_unique_words = std::stoi(line);
-  std::unordered_map<std::string, int> dict;
-  std::unordered_map<int, std::string> inv_dict;
-  for(int i = 0; i < n_unique_words; i++) {
-    std::getline(file, line);
-    dict[line]=i;
-    inv_dict[i]=line;
-  }
-  file.close();
-  double* term_freq = new double[n_unique_words]();
-  std::map<std::string, double*> index;
+
   int n_docs = 0;
-  for (const auto& entry : std::filesystem::directory_iterator("lemma/")) {
-    if (entry.path() == "lemma/dict.txt"){
-      continue;
+  std::unordered_map<int, int> doc_id_map;
+  {
+    const char* query = "SELECT doc_id FROM documents;";
+    sqlite3_stmt* stmt;
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    check_resut(rc);
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      int doc_id = sqlite3_column_int(stmt, 0);
+      doc_id_map[doc_id] = n_docs++;
+
     }
-    std::ifstream docfile(entry.path());
-    if(!docfile.is_open()) {
-      std::cerr << "Failed to open a file" << std::endl;
-      return 1;
-    }
-    n_docs++;
-    double* bag_of_words = new double[n_unique_words]();
-    bool has_seen_doc[n_unique_words] = {false};
-    while(std::getline(docfile, line)){
-      int idx = dict[line];
-      if (!has_seen_doc[idx]){
-        term_freq[idx] += 1;
-        has_seen_doc[idx] = true;
-      }
-      bag_of_words[idx] +=1;
-    }
-    docfile.close();
-    index[entry.path()] = bag_of_words;
+    sqlite3_finalize(stmt);
   }
-  for (int i = 0; i < n_unique_words; i++) {
-    double t = term_freq[i];
-    assert(t<=n_docs);
-    term_freq[i] = log((double)n_docs/t);
-  }
-  for (auto it = index.begin(); it != index.end(); ++it) {
-    double *bow = it->second;
-    for (int i = 0; i < n_unique_words; ++i) {
-      bow[i]*=term_freq[i];
+  std::cout << "Found " << n_docs << " documents." << std::endl;
+  int n_terms = 0;
+  std::unordered_map<int, int> word_id_map;
+  std::vector<double> inv_tf;
+  {
+    const char* query = "SELECT word_id, frequency FROM words;";
+    sqlite3_stmt* stmt;
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    check_resut(rc);
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      int word_id = sqlite3_column_int(stmt, 0);
+      int freq = sqlite3_column_int(stmt, 1);
+      word_id_map[word_id] = n_terms++;
+      double it = log((double)n_docs/(double)freq);
+      inv_tf.push_back(it);
     }
+    sqlite3_finalize(stmt);
   }
-  std::string q = "\"lemma/C_(programming_language)\"";
-  dist
-  for (auto it = index.begin(); it != index.end(); ++it) {
+  std::cout << "Found " << n_terms << " terms." << std::endl;
+  double *mem = new double[n_docs*n_terms];
+  double * bag_of_words[n_docs];
+  for (int i = 0; i < n_docs; i++) {
+    bag_of_words[i] = &mem[i*n_terms];
+  }
+  int terms_eval = 0;
+  {
+    const char* query = "SELECT doc_id, word_id, frequency FROM bag_of_words;";
+    sqlite3_stmt* stmt;
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    check_resut(rc);
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      int doc_id = sqlite3_column_int(stmt, 0);
+      int word_id = sqlite3_column_int(stmt, 1);
+      int freq = sqlite3_column_int(stmt, 2);
+      bag_of_words[doc_id_map[doc_id]][word_id_map[word_id]] = (double)freq * inv_tf[word_id_map[word_id]];
+      terms_eval++;
+    }
+    sqlite3_finalize(stmt);
+  }
+  for (int i = 0; i < n_docs; i++) {
+    std::cout << bag_of_words[i][0] << std::endl;
+  }
+
+  sqlite3_close(db);
+  delete[] mem;
   return 0;
-}k
+}
